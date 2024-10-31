e6c9939bfe673fb67c64c8a9802db03b8d5c58c5
llynoirx <kathyho04@gmail.com>
2024-10-31 03:26:27 -0400

    update about and fix main

--- routes/About.jsx
+++ routes/About.jsx
@@ -1,34 +1,19 @@
-// src/Components/About.js
 import React from 'react';
 
 const About = () => {
   return (
     <div className="about-container">
-      <h1>About Food Recipes App</h1>
+      <h1>Recipe Dashboard</h1>
       <p>
-        Welcome to the Food Recipes App, your ultimate destination for exploring
-        delicious recipes! Whether you are a beginner cook or a seasoned chef,
-        our app is designed to inspire you with a diverse collection of recipes
-        that cater to all tastes and dietary preferences.
+        Welcome to the Recipe Dashboard!
       </p>
       <h2>Features</h2>
       <ul>
-        <li>Search for recipes by title or dietary preference.</li>
-        <li>Filter recipes based on health scores and serving sizes.</li>
-        <li>Visualize data through interactive charts and graphs.</li>
-        <li>Save your favorite recipes for easy access.</li>
+        <li> Analyzes and filters categories such as cuisine, diet, and 
+             intolerances through search </li>
+        <li> Present a comprehensive summary of the recipes, including statistics like 
+             mean ready time, mean servings, and average health score rating</li>
       </ul>
-      <h2>Our Mission</h2>
-      <p>
-        Our mission is to promote healthy eating and cooking habits by providing
-        users with a wide range of recipes that are not only delicious but also
-        nutritious. We believe that cooking should be fun, easy, and accessible
-        to everyone!
-      </p>
-      <h2>Meet the Team</h2>
-      <p>
-        Kathy Ho
-      </p>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from './App.jsx'
 import DetailView from '../routes/DetailView';
+import About from '../routes/About';
 import NotFound from '../routes/NotFound';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  //   <BrowserRouter>
-  //     <Routes>
-  //       <Route path="/" element={<App />} /> 
-  //       <Route path="/about" element={<About/>} /> 
-  //       <Route path="/recipe/:id" element={<DetailView/>} /> 
-  //       <Route path="*" element={ <NotFound /> }/>
-  //     </Routes>
-  //   </BrowserRouter>
-  // </StrictMode>
   <StrictMode>
-  <App />
-</StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} /> 
+        <Route path="/about" element={<About/>} /> 
+        <Route path="/recipe/:id" element={<DetailView/>} /> 
+        <Route path="*" element={ <NotFound /> }/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
