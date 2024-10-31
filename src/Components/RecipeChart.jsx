99983f33b7b996598a008bb823520878a556508b
llynoirx <kathyho04@gmail.com>
2024-10-31 04:59:56 -0400

    fix graph css

--- src/Components/RecipeChart.jsx
+++ src/Components/RecipeChart.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import './RecipeChart.css';
 
 const RecipeChart = ({ recipeData }) => {
-
+    const CustomTooltip = ({ active, payload }) => {
+        if (active && payload && payload.length) {
+            return (
+                <div className="custom-tooltip">
+                    <h4>{payload[0].payload.title}</h4> {/* Recipe title */}
+                    <p>Health Score: {payload[0].value}</p> {/* Health score */}
+                </div>
+            );
+        }
+        return null;
+    };
 
     return (
         <div className="chart-container">
             <h3 className="chart-title">Recipe Health Score</h3>
-            <LineChart 
+            <BarChart 
                 width={400} 
                 height={400} 
                 margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
                 data={recipeData}>
-             
-            <Line 
-                type="monotone"
-                dataKey="healthScore"
-                stroke="#ff7300" />
-              <CartesianGrid stroke="red" strokeDasharray="5 5" />
-              <XAxis dataKey="title" tick={{ fill: 'white' }} />
-              <YAxis tick={{ fill: 'white' }} />
-              <Tooltip />
-            </LineChart>
+                
+                <CartesianGrid strokeDasharray="5 5" />
+                <YAxis tick={{ fill: 'green' }} />
+                <Tooltip content={<CustomTooltip />} /> 
+                <Legend />
+                
+                <Bar dataKey="healthScore" fill="green" />
+            </BarChart>
         </div>
     );
 };
